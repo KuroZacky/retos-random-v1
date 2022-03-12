49bf212553f50b881a82b83ba815de9e5745d8df
saul-montero <saul.sandoval@workrise.com>
2022-03-12 12:39:40 -0600

    Agregando retos

--- src/App.js
+++ src/App.js
@@ -8,13 +8,22 @@ function App() {
   const retos = [
     "Tener una conversación con una persona aleatoria",
     "Pedirle el número de teléfono a alguien",
+    "Shot",
     "Abraza a la primera persona que encuentres",
     "Grita que estas enamorad@",
+    "Besa a alguien en el cuello",
+    "Shot",
+    "Fondo a tu bebida",
+    "Dar un beso en la boca a un desconocido",
+    "Shot",
+    "gritar: «Hago un oral gratis»",
   ];
+  
   const [indice, setIndice] = React.useState(
     Math.floor(Math.random() * (retos.length - 0) + 0)
   );
-  const nuevoReto = () => setIndice(Math.floor(Math.random() * (4 - 0) + 0));
+
+  const nuevoReto = () => setIndice(Math.floor(Math.random() * (retos.length - 0) + 0));
 
   return (
     <div className="App App-header">
