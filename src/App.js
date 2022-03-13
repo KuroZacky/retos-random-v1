61b9b8e0758a53a8b6cd4834a7dd7f5b676c8d82
saul-montero <saul.sandoval@workrise.com>
2022-03-12 21:50:17 -0600

    Agregando estilos craperos

--- src/App.js
+++ src/App.js
@@ -1,30 +1,77 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
-import Fingerprint from "@mui/icons-material/Fingerprint";
+import Cached from "@mui/icons-material/Cached";
 import "./App.css";
 
 function App() {
   const retos = [
-    "Tener una conversación con una persona aleatoria",
-    "Pedirle el número de teléfono a alguien",
+    "Tener una conversación con un desconocido",
+    "Saca a bailar a un desconocido",
+    "Pedirle el número de teléfono a un desconocido",
     "Shot",
     "Abraza a la primera persona que encuentres",
     "Grita que estas enamorad@",
+    "Da una nalgada a alguien",
     "Besa a alguien en el cuello",
     "Shot",
+    "Saca a bailar a un amigo",
     "Fondo a tu bebida",
-    "Dar un beso en la boca a un desconocido",
+    "Da un beso a un desconocido",
     "Shot",
-    "gritar: «Hago un oral gratis»",
+    "Grita: «Hago un oral gratis»",
+    "Besa a una chica",
+    "Cachetada a alguien (solo si eres chica)",
+    "Saca a bailar a un amigo",
+    "Fondo a tu bebida",
+    "Pide una bebida especial",
+    "Haz el 4",
+    "¿Otra botella?",
+    "Pide MOET",
+    "Invitale un shot a alguien",
+    "Decide quien toma",
+    "De la mesa, ¿a quien le darías?",
+    "Baila con la persona a tu derecha",
+    "Baila con la persona a tu izquierda",
+    "Comparte un shot con quien quieras (ambos toman)",
+    "Baila sexy",
+    "Da tu número de cel a alguien",
+    "¿Posición sexual favorita?",
+    "Regala un beso",
+    "Despeina a alguien",
+    "Muerde a alguien",
+    "Da una nalgada a alguien",
+    "Ronda de shots",
+    "Sacudele la cabeza a alguien",
   ];
 
   const [indice, setIndice] = React.useState(
     Math.floor(Math.random() * (retos.length - 0) + 0)
   );
 
-  const nuevoReto = () =>
+  const [timer, setTimer] = React.useState(3);
+  const [showTimer, setShowTimer] = React.useState(false);
+
+  const updateTimer = (timer) => {
+    setTimeout(() => {
+      if (timer === 0) {
+        setShowTimer(false);
+        return;
+      }
+      if (timer - 1 > 0) {
+        setTimer((prevState) => --prevState);
+        updateTimer(timer);
+      }
+     } , 1000)
+  }
+
+  const nuevoReto = () => {
+    setShowTimer(true);
+    updateTimer(timer);
     setIndice(Math.floor(Math.random() * (retos.length - 0) + 0));
+  }
+
+
 
   return (
     <div className="App App-header">
@@ -38,16 +85,17 @@ function App() {
           gap: 2,
         }}
       >
+        <h2>LA CULPABLE - Centro</h2>
         <h1>Reto:</h1>
         <h2>{retos[indice]}</h2>
         <Button
           onClick={nuevoReto}
-          color="success"
+          color="error"
           sx={{ maxWidth: "300px", fontWeight: "bold" }}
-          variant="contained"
-          endIcon={<Fingerprint />}
+          variant="outlined"
+          endIcon={<Cached />}
         >
-          <h3>OTRO</h3>
+          {<h3>SIGUIENTE</h3>}
         </Button>
       </Box>
     </div>
